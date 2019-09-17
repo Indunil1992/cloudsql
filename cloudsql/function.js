b5f5d7af9b588c64a30319f0035cd3838cb62882
Indunil1992 <indunil@adroitlogic.com>
2019-09-17 15:00:33 +0530

    a

--- cloudsql/function.js
+++ cloudsql/function.js
@@ -1,23 +1,27 @@
-let SL_GCP = require('slappforge-sdk-gcp');
-let sqlConnMgr = require('./SqlConnMgr');
-const sql = new SL_GCP.SQL(sqlConnMgr);
+let google = require('googleapis').google;
+let _auth = require('./Authorizer');
+const datastore = google.datastore('v1');
 
 exports.handler = function (request, response) {
-    // You can pass the existing connection to this function.
-    // A new connection will be created if it's not present as the third param.
-    // You must always end/destroy the DB connection after it's used.
-    sql.query({
-        instanceIdentifier: 'instanceid1',
-        query: 'INSERT INTO Client(id,age,city);',
-        inserts: [12, 28, "kandy"]
-    }, function (error, results, connection) {
-        if (error) {
-            throw error;
-        } else {
-            // use `results` to do your magic
+    datastore.projects.beginTransaction({
+        projectId: process.env.GCP_PROJECT,
+        resource: {
+            transactionOptions: {
+                readWrite: {}
+            }
         }
-        connection.end();
-    });
+    }).then(response => {
+        console.log(response.data);           // successful response
+        /*
+        response.data = {
+            "transaction": "<transaction ID>"
+        }
+        */
+    })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
 
     response.send({ "message": "Successfully executed" });
 
