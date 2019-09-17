0d505b98dd36dd496b482504f08442b96a972048
Indunil1992 <indunil@adroitlogic.com>
2019-09-17 14:58:00 +0530

    quary

--- cloudsql/function.js
+++ cloudsql/function.js
@@ -3,15 +3,22 @@ let sqlConnMgr = require('./SqlConnMgr');
 const sql = new SL_GCP.SQL(sqlConnMgr);
 
 exports.handler = function (request, response) {
+    // You can pass the existing connection to this function.
+    // A new connection will be created if it's not present as the third param.
     // You must always end/destroy the DB connection after it's used.
-    sql.beginTransaction({
-        instanceIdentifier: 'instanceid1'
-    }, function (error, connection) {
+    sql.query({
+        instanceIdentifier: 'instanceid1',
+        query: 'INSERT INTO Client(id,age,city);',
+        inserts: [12, 28, "kandy"]
+    }, function (error, results, connection) {
         if (error) {
             throw error;
+        } else {
+            // use `results` to do your magic
         }
+        connection.end();
     });
-connection.end();
+
     response.send({ "message": "Successfully executed" });
 
 }
